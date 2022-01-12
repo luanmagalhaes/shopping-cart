59a44ec616f95ca0e2e6c383df090042b747ae4f
pabloherzberg <m.agalhaes@hotmail.com>
2022-01-12 20:17:50 -0300

    🎨 add colors to colors.tsx

--- src/assets/colors.tsx
+++ src/assets/colors.tsx
@@ -3,7 +3,9 @@ const colors = {
     blue: '#2264D1',
     darkBlue: '#1671C0',
     darkGray: '#787885',
+    lightGray: '#5E6366',
     gray: '#EDEDF0',
+    graybg: '#F9F9F9',
     white: '#FFF',
 };
 
